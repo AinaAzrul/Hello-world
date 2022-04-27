757a5dc903fdcde73675c312c0e61554ab835ba7
Aina Diyanah <ainardm22@gmail.com>
2022-04-27 11:06:38 +0800

    comm-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world';
+  title  = 'Welcome to the Angular world';
+  myName = 'Ainoo';
 }
